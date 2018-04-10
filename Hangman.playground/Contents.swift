b012738f1570e03bf2c553f21420db1b64c61661
Henry Allsuch <henry@agilepixel.io>
2018-04-10 09:27:19 +0100

    adding some vars

--- Hangman.playground/Contents.swift
+++ Hangman.playground/Contents.swift
@@ -1,7 +1,33 @@
 
 import Foundation
 
-let totalNumberOfGuesses = 6;
-var numberOfGuessesLeft = 6;
+class Game {
+    
+    let totalNumberOfGuesses = 6;
+    var numberOfGuessesLeft = 6;
+    var misses:[String] = []
+    
+}
+
+let wordToGuess = "cat"
+let wordLength = wordToGuess.count
+
+func makeAGuess(_ Letter : String) -> Bool {
+    
+    return wordToGuess.contains(Letter)
+}
 
 
+let firstGuess = "A"
+let firstAnswer: Bool = makeAGuess(firstGuess)
+
+
+new Game
+
+func generateGuess() -> String {
+    
+    return firstGuess
+}
+
+
+firstAnswer
