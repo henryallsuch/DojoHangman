d38d37ab8834c4582f59100de21555efabc39fcd
Henry Allsuch <henry@agilepixel.io>
2018-04-10 23:22:40 +0100

    • building out class

--- Hangman.playground/Contents.swift
+++ Hangman.playground/Contents.swift
@@ -1,33 +1,83 @@
 
 import Foundation
 
-class Game {
+class Hangman {
     
-    let totalNumberOfGuesses = 6;
-    var numberOfGuessesLeft = 6;
+    let totalNumberOfGuesses = 3
+    var numberOfGuessesLeft = 3
+    
+//    init() {
+//
+//        numberOfGuessesLeft = totalNumberOfGuesses
+//
+//    }
+    func makeAGuess(_ Letter : String) -> Bool {
+        
+        return wordToGuess.contains(Letter)
+    
+    }
+    
+    func isGameOver() -> Bool{
+        
+        if(self.numberOfGuessesLeft == 0){
+            
+            return true;
+            
+        } else {
+            
+            self.numberOfGuessesLeft -= 1
+            
+            return false
+            
+        }
+    
+    }
+
+    
+}
+
+class Player {
+    
     var misses:[String] = []
+    var hits:[String] = []
+    
+    var wordLength : Int = 0
+    
+    init(_ length: Int) {
+        self.wordLength = length
+    }
+    
+    func generateGuess() -> String {
+        
+        let firstGuess = "a"
+        return firstGuess
+    }
+    
+    func recordTheResult(_ result:Bool, forGuess: String) {
+    
+    
+    }
+    
+    func recordWordLength(_ length: Int){
+        self.wordLength = length
+    }
+    
+    
     
 }
 
 let wordToGuess = "cat"
 let wordLength = wordToGuess.count
 
-func makeAGuess(_ Letter : String) -> Bool {
+var newGame = Hangman()
+
+let player = Player(wordLength)
+
+while !newGame.isGameOver()  {
     
-    return wordToGuess.contains(Letter)
+   let guess = player.generateGuess()
+    player.recordTheResult(newGame.makeAGuess(guess), forGuess: guess)
+    
 }
 
 
-let firstGuess = "A"
-let firstAnswer: Bool = makeAGuess(firstGuess)
-
-
-new Game
-
-func generateGuess() -> String {
-    
-    return firstGuess
-}
-
-
-firstAnswer
