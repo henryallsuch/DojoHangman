19ec1b92f42fd532d565d4c76f42f81ca27b8efe
Henry Allsuch <henry@agilepixel.io>
2018-04-11 20:25:41 +0100

    • making guesses to api!

--- Hangman.playground/Contents.swift
+++ Hangman.playground/Contents.swift
@@ -1,5 +1,15 @@
 
 import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+struct Answer {
+    var found : Bool
+    var atIndex: String.Index? = nil
+}
+
+
 
 class Hangman {
     
@@ -16,9 +26,21 @@ class Hangman {
         self.numberOfGuessesLeft = self.totalNumberOfGuesses
 
     }
-    func makeAGuess(_ Letter : String) -> Bool {
+    func makeAGuess(_ Letter : Character) -> Answer {
         
-        return wordToGuess.contains(Letter)
+        
+        let api = Api()
+        
+        api.makeGuess()
+        
+//        if let index = wordToGuess.index(of: Letter) {
+//
+//            return Answer(found:true, atIndex: index)
+//
+//        }
+//
+//        return Answer(found:false, atIndex: nil)
+        return Answer(found:false, atIndex: nil)
     
     }
     
@@ -43,7 +65,7 @@ class Hangman {
 
 class Player {
     
-    var misses:[String] = []
+    var misses:[Character] = []
     var hits = [Int: String]()
     
     var currentGame : Hangman
@@ -52,19 +74,19 @@ class Player {
         self.currentGame = game
     }
     
-    func generateGuess() -> String {
+    func generateGuess() -> Character {
         
-        let firstGuess = "a"
+        let firstGuess:Character = "a"
         return firstGuess
     }
     
-    func recordTheResult(_ result:Bool, forGuess: String) {
+    func recordTheResult(_ result:Answer, forGuess: Character) {
         
-        print(result)
+        print(result.found)
         
-        if(result){
+        if(result.found){
             
-           // hits.append(String)
+          // hits.append(forGuess)
             
         } else {
             
@@ -74,15 +96,79 @@ class Player {
 
 }
 
+class Api {
+    
+    //let endpointUrl
+    
+    var scheme: String = ""
+    var baseUrl: String = ""
+    
+    var token : String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhY2U1MWRiNWU3OTM4MDAxNDBhMjYwNyIsImlhdCI6MTUyMzQ3MDkzMCwiZXhwIjoxNTIzNTU3MzMwfQ.cxsvZYSkW0nk7qa0kckhj7fgXmCC0ooU1CsjARK09nM"
+    
+    func login(){
+    
+    }
+    
+    func makeGuess(){
+        
+        let parameters = ["letter": "z"] as [String : Any]
+        
+        do {
+            let postData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            
+            let requestBody = postData as Data
+            
+            makeRequest(method: "PATCH", body: requestBody)
+            
+        } catch {
+           print("failed to parse json")
+        }
+    
+       
+        
+    }
+    
+    func makeRequest( method :String, body : Data ){
+        
+        
+        let headers = [
+            "Content-Type": "application/json",
+            "x-access-token": token
+        ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://dojo-hangman-server.herokuapp.com/api/games/current")! as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        
+        request.httpMethod = method
+        request.allHTTPHeaderFields = headers
+        request.httpBody = body
+    
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            if let data = data, let contents = String(data: data, encoding: String.Encoding.utf8) {
+                print(contents)
+            }
+        }
+        task.resume()
+        
+    }
+}
+
+
 var newGame = Hangman("cat")
 
 let player = Player(newGame)
 
+
+
 while !newGame.isGameOver()  {
-    
+
    let guess = player.generateGuess()
     player.recordTheResult(newGame.makeAGuess(guess), forGuess: guess)
-    
+
 }
 
 
